4cbd490a5dc9eccba6875a3849ae6061ffcd0fbf
hadijah <hadijah315@gmail.com>
2017-11-08 18:13:57 -0800

    api

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from models import db
+
+app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': '0000',
+    'db': 'yummyapi',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db.init_app(app)
+
+
+if __name__ == '__main__':
+    app.run()
+
